dce8fac145a178b0166b489839bb2f8422b4c127
Francesco Laterza <francescolaterza00@gmail.com>
2024-11-23 11:07:55 +0100

    change deploy url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://radicadev.github.io",
+  url: "https://docs.radica.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
